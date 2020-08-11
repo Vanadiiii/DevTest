882d1693ea4f3192f1d882c9a3c9d22093211bd8
Vanadiiii <54495915+Vanadiiii@users.noreply.github.com>
2020-08-11 12:12:51 +0400

    Update CommandClear.java

--- src/main/java/ru/imatveev/application/command/CommandClear.java
+++ src/main/java/ru/imatveev/application/command/CommandClear.java
@@ -7,9 +7,24 @@ public class CommandClear extends AbstractCommand {
 
     @Override
     public void execute(String argument, Context context) {
-        context.getArrayMap()
-                .entrySet()
-                .forEach(entry -> entry.setValue(new int[0]));
-        System.out.println("all arrays are cleared");
+        if (argument.isBlank()) {
+            context.getArrayMap()
+                    .entrySet()
+                    .forEach(entry -> entry.setValue(new int[0]));
+            System.out.println("all arrays were cleared");
+        } else {
+            ArrayType type;
+            try {
+                type = ArrayType.valueOf(argument.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new CustomException("unknown type of array - '" + argument + "'");
+            }
+            context.getArrayMap()
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> type.equals(entry.getKey()))
+                    .forEach(entry -> entry.setValue(new int[0]));
+            System.out.println("array" + type.name() + " was cleared");
+        }
     }
 }
